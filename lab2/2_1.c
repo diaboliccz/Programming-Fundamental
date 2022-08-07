f19e2c7ae0250ac1d93eb8f70b8517949956d26f
Kolawat Inpan <dominolity@gmail.com>
2022-08-07 08:23:17 +0700

    edit

--- lab2/2_1.c
+++ lab2/2_1.c
@@ -5,14 +5,8 @@ int main(){
     printf("Enter number : ");
     scanf("%d",&num);
     result=num;
-    for(int i=1;i<result-1;i++){
-        if((num-1)%i==0){
-            printf("%d",i);
-            do{
-                printf("x");
-                num/=i;
-            }while()
-        }
+    for(int i=0;i<result;i++){
+        
     }
     return 0;
 }
