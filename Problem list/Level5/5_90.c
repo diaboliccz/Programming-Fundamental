6cb88db57f4d8e54f569b605411caf5c208bc3e3
Kolawat Inpan <dominolity@gmail.com>
2022-08-08 16:12:04 +0700

    add .gitignore

---
+++ Problem list/Level5/5_90.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+double sqrt(double num){
+    double x_0 = num/2;
+    double x_1;
+    double precision = .00000001;
+    double difference = x_1 - x_0;
+    while (difference >= precision){
+        x_1 = .5*(x_0 + num/x_0);
+    }
+    return x_1;
+}
+
+int main(){
+    printf("%.10f",sqrt(111000));
+    return 0;
+}
